a789f11aa41b0bc4967fb941e2884872fdd94c15
MatrixCW <cuiwei.exe@gmail.com>
2013-01-23 11:39:27 +0800

    add Game

---
+++ PS2/Game/Game/ViewController.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  Game
+//
+//  Created by Cui Wei on 1/23/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+
+- (IBAction)buttonPressed:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIScrollView *gamearea;
+
+
+@end
