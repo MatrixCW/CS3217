ec75193282470ab7cf2318089c27e43f603d9602
MatrixCW <cuiwei.exe@gmail.com>
2013-01-31 23:10:35 +0800

    re-add

---
+++ PS3/Game/Game/ViewController+Extension.m
@@ -0,0 +1,29 @@
+//
+//  ViewController+Extension.m
+//  Game
+//
+//  Created by Cui Wei on 1/31/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "ViewController+Extension.h"
+
+@implementation ViewController (Extension)
+- (void)save {
+    NSLog(@"I can save");
+    [self say];
+    
+}
+
+
+- (void)load{
+    NSLog(@"I can load");
+}
+
+
+
+- (void)reset{
+    NSLog(@"I can reset");
+}
+
+@end
